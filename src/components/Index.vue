08ecb30b8d031b91f7faa282bd47f32861e2df12
Andrés Ávila <andaviaco@gmail.com>
2017-08-23 16:41:53 -0500

    create Index and ProcessForm components

---
+++ src/components/Index.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <h1>OS Simulator</h1>
+    <process-form></process-form>
+  </div>
+</template>
+
+<script>
+import ProcessForm from '@/components/ProcessForm';
+
+export default {
+  name: 'index',
+  data() {
+    return {};
+  },
+  components: {
+    ProcessForm,
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+/*h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}*/
+</style>
---
+++ src/components/ProcessForm.vue
@@ -0,0 +1,141 @@
+<template>
+  <form @submit.prevent="handleSubmitForm">
+    <div class="field is-horizontal">
+      <div class="field-label">
+        <label
+          class="label"
+          for="program_id"
+        >
+          Num. Programa (ID)
+        </label>
+      </div>
+      <div class="field-body">
+        <input
+          id="program_id"
+          class="input"
+          type="text"
+          v-model="process.id"
+        >
+      </div>
+    </div>
+
+    <div class="field is-horizontal">
+      <div class="field-label">
+        <label
+          class="label"
+          for="programmer-name"
+        >
+          Nombre del Programador
+        </label>
+      </div>
+      <div class="field-body">
+        <input
+          id="programmer-name"
+          class="input"
+          type="text"
+          v-model="process.programmerName"
+        >
+      </div>
+    </div>
+
+    <div class="field is-horizontal">
+      <div class="field-label">
+        <label class="label" for="operation">Operación</label>
+      </div>
+      <div class="field-body">
+        <div class="field is-narrow">
+          <input
+            id="operation"
+            class="input"
+            type="number"
+            v-model="process.operation.operad1"
+          >
+        </div>
+
+        <div class="control">
+          <div class="select">
+            <select v-model="process.operation.operator">
+              <option value="+">+</option>
+              <option value="-">-</option>
+              <option value="x">×</option>
+              <option value="/">÷</option>
+              <option value="%">mod</option>
+            </select>
+          </div>
+        </div>
+
+        <div class="field is-narrow">
+          <input
+            id="operation"
+            class="input"
+            type="number"
+            v-model="process.operation.operand2"
+          >
+        </div>
+      </div>
+    </div>
+
+    <div class="field is-horizontal">
+      <div class="field-label">
+        <label
+          class="label"
+          for="time-max"
+        >
+          Tiempo Maximo (segundos)
+        </label>
+      </div>
+      <div class="field-body">
+        <input
+          id="time-max"
+          class="input"
+          type="number"
+        >
+      </div>
+    </div>
+
+    <div class="field is-horizontal">
+      <div class="field-label">
+      </div>
+      <div class="field-body">
+        <button class="button is-primary" type="submit">
+          <span class="icon is-small">
+            <i class="fa fa-plus"></i>
+          </span>
+          <span>Agregar</span>
+        </button>
+      </div>
+    </div>
+
+    <pre>{{ process }}</pre>
+  </form>
+</template>
+
+<script>
+export default {
+  name: 'process-form',
+  data() {
+    return {
+      process: {
+        id: '',
+        programmerName: '',
+        timeMax: '',
+        operation: {
+          operad1: 1,
+          operator: '+',
+          operand2: 1,
+        },
+      },
+    };
+  },
+  methods: {
+    handleSubmitForm() {
+      console.log('submit');
+    },
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+
+</style>
