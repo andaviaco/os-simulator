c6943797841912d67776ea09526c2535efd273da
Andrés Ávila <andaviaco@gmail.com>
2017-08-27 02:54:56 -0500

    move ProgramBatcher to the models diirectory

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -41,50 +41,7 @@
 import ProcessForm from '@/components/ProcessForm';
 import ProcessBatches from '@/components/ProcessBatches';
 
-class ProgramBatcher {
-  MAX_PROGRAM_PER_BARCH = 5;
-
-  constructor() {
-    this.batches = [[
-      {
-        id: '55',
-        programmerName: 'andres',
-        timeMax: '56',
-        operation: {
-          operad1: 5,
-          operator: '*',
-          operand2: 6,
-        },
-      },
-    ]];
-  }
-
-  get lastBatch() {
-    return this.batches[this.batches.length - 1];
-  }
-
-  addProgram(program) {
-    const lastBatch = this.lastBatch;
-
-
-    if (lastBatch.length + 1 < this.MAX_PROGRAM_PER_BARCH) {
-      lastBatch.push(program);
-    } else {
-      this.createBatch(program);
-    }
-  }
-
-  createBatch(...programs) {
-    this.batches.push(programs);
-  }
-
-  isIdAvailable(id) {
-    const flatPrograms = this.batches.reduce((acc, batch) => acc.concat(batch), []);
-    const takenIds = flatPrograms.map(program => program.id);
-
-    return !takenIds.find(programId => programId === id);
-  }
-}
+import ProgramBatcher from '@/models/ProgramBatcher';
 
 
 const batcher = new ProgramBatcher();
