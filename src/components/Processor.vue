7bb6b19e318d3d90b09ea02b15e3aef37e6dc9db
Andrés Ávila <andaviaco@gmail.com>
2017-08-27 04:10:49 -0500

    change processor header for a level

--- src/components/Processor.vue
+++ src/components/Processor.vue
@@ -1,40 +1,46 @@
 <template lang="html">
   <div>
-    <div class="">
-      <button
-        class="button is-success"
-        type="button"
-        @click="handleStartClick"
-      >
-        <span class="icon">
-          <i class="fa fa-flash"></i>
-        </span>
-        <span>
-          Procesar
-        </span>
-      </button>
-
-      <span class="is-pulled-right">
-        <div class="field is-grouped ">
-          <div class="control">
-            <div class="tags has-addons">
-              <span class="tag is-dark">Lotes restantes:</span>
-              <span class="tag is-warning">
-                {{ this.batches.length }}
+    <div class="level">
+        <div class="level-left">
+          <div class="level-item">
+            <button
+              class="button is-success"
+              type="button"
+              @click="handleStartClick"
+              >
+              <span class="icon">
+                <i class="fa fa-flash"></i>
               </span>
-            </div>
+              <span>
+                Procesar
+              </span>
+            </button>
           </div>
+      </div>
 
-          <div class="control">
-            <div class="tags has-addons">
-              <span class="tag is-dark">Tiempo transcurrido:</span>
-              <span class="tag is-info">
-                <stopwatch ref="timer"></stopwatch>
-              </span>
+      <div class="level-right">
+        <div class="leve-item">
+          <div class="field is-grouped ">
+            <div class="control">
+              <div class="tags has-addons">
+                <span class="tag is-dark">Lotes restantes:</span>
+                <span class="tag is-warning">
+                  {{ this.batches.length }}
+                </span>
+              </div>
+            </div>
+
+            <div class="control">
+              <div class="tags has-addons">
+                <span class="tag is-dark">Tiempo transcurrido:</span>
+                <span class="tag is-info">
+                  <stopwatch ref="timer"></stopwatch>
+                </span>
+              </div>
             </div>
           </div>
         </div>
-      </span>
+      </div>
     </div>
   </div>
 </template>
