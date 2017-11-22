4303fef685ff272f453467199d214a9c4214a72c
Andrés Ávila <andaviaco@gmail.com>
2017-11-22 03:21:48 -0600

    implement method to get a position given the required amount of memory

--- src/models/Memory.js
+++ src/models/Memory.js
@@ -8,4 +8,43 @@ export default class Memory {
     ));
   }
 
+  /**
+   * Get available position given a required space
+   * @param  {int}    memory required memory space
+   * @return {int}           available space index. Null if no available space.
+   */
+  freePosition(memory) {
+    const spaces = [];
+    const empties = [...this.data.entries()]
+      .filter(([, frame]) => frame[0] === null)
+      .map(([i]) => i);
+
+    let freePosition;
+
+    for (let i = 0; i < empties.length; i++) { // eslint-disable-line no-plusplus
+      const frameIndex = empties[i];
+      let spaceCount = 1;
+
+
+      for (let j = i; j < empties.length; j++) { // eslint-disable-line no-plusplus
+        const currentIndex = empties[j];
+        const nextIndex = empties[j + 1];
+
+        if (j + 1 === empties.length || nextIndex - currentIndex !== 1) {
+          i = j;
+          break;
+        }
+
+        spaceCount += 1;
+      }
+
+      spaces.push([frameIndex, spaceCount * this.frameSize]);
+    }
+
+    freePosition = spaces.find(([, space]) => memory <= space);
+    freePosition = freePosition ? freePosition[0] : null;
+
+    return freePosition;
+  }
+
 }
