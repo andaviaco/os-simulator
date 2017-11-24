45fe6f62a7c0f1ba04a93b006a0602d7d6fb963f
Andrés Ávila <andaviaco@gmail.com>
2017-11-23 19:47:37 -0600

    change insert method to allow store the process in non-contiguous frames

--- src/models/Memory.js
+++ src/models/Memory.js
@@ -7,13 +7,11 @@ export default class Memory {
   }
 
   add(pid, memory) {
-    const freePosition = this.freePosition(memory);
-
-    if (freePosition === null) {
+    if (!this.fits(memory)) {
       return null;
     }
 
-    return this.insert(freePosition, memory, pid);
+    return this.insert(memory, pid);
   }
 
   remove(pid) {
@@ -27,74 +25,48 @@ export default class Memory {
     }
   }
 
-  insert(frameIndex, memory, pid) {
+  insert(memory, pid) {
+    const freeFrames = this.freeFrames();
     const wholeFrames = Math.floor(memory / this.frameSize);
-    const pratialFrame = memory % this.frameSize;
-    const lastPosition = frameIndex + wholeFrames;
+    const pratialFrameSpace = memory % this.frameSize;
 
+    let partialFrameIndex;
+    let frameIndex = [];
 
-    /* eslint-disable */
-    for (let i = frameIndex; i < lastPosition && i < this.data.length; i++) {
+    frameIndex = freeFrames.slice(0, wholeFrames).map((i) => {
       const newData = Array.from(Array(this.frameSize), () => pid);
 
       this.data.splice(i, 1, newData);
-    }
-    /* eslint-enable */
 
-    if (lastPosition < this.data.length) {
-      const frame = this.data[lastPosition];
+      return i;
+    });
 
-      for (let i = 0; i < pratialFrame; i++) { // eslint-disable-line no-plusplus
-        frame[i] = pid;
+    if (pratialFrameSpace) {
+      partialFrameIndex = freeFrames[wholeFrames];
+
+      frameIndex.push(partialFrameIndex);
+
+      for (let i = 0; i < pratialFrameSpace; i++) { // eslint-disable-line no-plusplus
+        this.data[partialFrameIndex].splice(i, 1, pid);
       }
     }
 
     return frameIndex;
   }
 
-  /**
-   * Get available position given a required space
-   * @param  {int}    memory required memory space
-   * @return {int}           available space index. Null if no available space.
-   */
-  freePosition(memory) {
-    const spaces = [];
+  freeFrames() {
     const empties = [...this.data.entries()]
       .filter(([, frame]) => frame[0] === null)
       .map(([i]) => i);
 
-    let freePosition;
-
-    for (let i = 0; i < empties.length; i++) { // eslint-disable-line no-plusplus
-      const frameIndex = empties[i];
-      let spaceCount = 1;
-
-
-      for (let j = i; j < empties.length; j++) { // eslint-disable-line no-plusplus
-        const currentIndex = empties[j];
-        const nextIndex = empties[j + 1];
-
-        if (j + 1 === empties.length || nextIndex - currentIndex !== 1) {
-          i = j;
-          break;
-        }
-
-        spaceCount += 1;
-      }
-
-      spaces.push([frameIndex, spaceCount * this.frameSize]);
-    }
-
-    freePosition = spaces.find(([, space]) => memory <= space);
-    freePosition = freePosition ? freePosition[0] : null;
-
-    return freePosition;
+    return empties;
   }
 
   fits(memory) {
-    const freePosition = this.freePosition(memory);
+    const freeFrames = this.freeFrames();
+    const memFrames = this.toFrames(memory);
 
-    if (freePosition !== null) {
+    if (freeFrames.length >= memFrames) {
       return true;
     }
 
@@ -103,5 +75,9 @@ export default class Memory {
 
   createEmptyFrame() {
     return Array.from(Array(this.frameSize), () => null);
+  }
+
+  toFrames(memory) {
+    return Math.ceil(memory / this.frameSize);
   }
 }
